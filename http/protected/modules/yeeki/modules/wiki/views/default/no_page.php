e5963c24d6c783fdff8ef2b5c194ab0ee325eed1
schuka-kirill <schuka.kirill@gmail.com>
2012-09-06 15:37:49 +0600

    united version (yii+bitrix)

--- http/protected/modules/yeeki/modules/wiki/views/default/no_page.php
+++
@@ -1,2 +0,0 @@
-<?php echo Yii::t('wiki', 'There is no page yet.')?>
-<?php echo CHtml::link(Yii::t('wiki', 'Create'), array('edit', 'uid' => $uid))?>?
--- http/themes/backend_bootstrap/BootstrapTheme.php
+++
@@ -1,8 +0,0 @@
-<?php
-    Yii::app()->setComponent('bootstrap', Yii::createComponent(array(
-        'class'=>$themeBase.'.extensions.bootstrap.components.Bootstrap',
-    )));
-    
-    Yii::setPathOfAlias('bootstrap', Yii::app()->theme->basePath.'/extensions/bootstrap');
-    Yii::app()->preload[] = 'bootstrap';
-?>
